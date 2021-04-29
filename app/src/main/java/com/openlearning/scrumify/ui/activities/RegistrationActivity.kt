553fb3be9a18c01cb8cc994a6de1c7b776b33df4
UsamaEjaz0 <uejaz0@gmail.com>
2021-04-29 05:16:30 +0500

    Added registeration activity

---
+++ app/src/main/java/com/openlearning/scrumify/ui/activities/RegistrationActivity.kt
@@ -0,0 +1,12 @@
+package com.openlearning.scrumify.ui.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.openlearning.scrumify.R
+
+class RegistrationActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_registration)
+    }
+}
